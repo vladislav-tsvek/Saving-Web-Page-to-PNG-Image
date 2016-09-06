a38bc78a3429277dd400337d49d0d328f7555710
vladislav.tsvek <vladislav.tsvek@teamdev.com>
2016-09-06 18:34:19 +0300

    Save Web page as an image by pressing Load. Load site by typing URL in textBox and press Enter. Dispose browser after the application is closed.

--- Saving Web Page to PNG Image/Form1.Designer.cs
+++ Saving Web Page to PNG Image/Form1.Designer.cs
@@ -28,12 +28,72 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripAddress = new System.Windows.Forms.ToolStripTextBox();
+            this.toolStripUpload = new System.Windows.Forms.ToolStripButton();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripLabel1,
+            this.toolStripAddress,
+            this.toolStripUpload});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(852, 25);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(77, 22);
+            this.toolStripLabel1.Text = "Enter address";
+            // 
+            // toolStripAddress
+            // 
+            this.toolStripAddress.Name = "toolStripAddress";
+            this.toolStripAddress.Size = new System.Drawing.Size(500, 25);
+            this.toolStripAddress.KeyDown += new System.Windows.Forms.KeyEventHandler(this.toolStripAddress_KeyDown);
+            // 
+            // toolStripUpload
+            // 
+            this.toolStripUpload.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripUpload.Image = ((System.Drawing.Image)(resources.GetObject("toolStripUpload.Image")));
+            this.toolStripUpload.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripUpload.Name = "toolStripUpload";
+            this.toolStripUpload.Size = new System.Drawing.Size(37, 22);
+            this.toolStripUpload.Text = "Load";
+            this.toolStripUpload.ToolTipText = "Load";
+            this.toolStripUpload.Click += new System.EventHandler(this.toolStripUpload_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(852, 469);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.Windows.Forms.ToolStripTextBox toolStripAddress;
+        private System.Windows.Forms.ToolStripButton toolStripUpload;
     }
 }
 
--- Saving Web Page to PNG Image/Form1.cs
+++ Saving Web Page to PNG Image/Form1.cs
@@ -1,10 +1,16 @@
-﻿using System;
+﻿using DotNetBrowser;
+using DotNetBrowser.Events;
+using DotNetBrowser.WinForms;
+using DotNetBrowser.WPF;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,9 +18,108 @@ namespace Saving_Web_Page_to_PNG_Image
 {
     public partial class Form1 : Form
     {
+
+        BrowserView browserView = new WinFormsBrowserView();
+
         public Form1()
         {
             InitializeComponent();
+
+            Controls.Add((Control)browserView);
+        }
+
+        private void toolStripAddress_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                browserView.Browser.LoadURL(toolStripAddress.Text.ToString());
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            browserView.Browser.LoadURL("http://www.google.com");
+            toolStripAddress.Text = browserView.Browser.URL.ToString();
+        }
+
+        private void toolStripUpload_Click(object sender, EventArgs e)
+        {
+            //Create new browser for rendering
+            WinFormsBrowserView browserView1 = new WinFormsBrowserView(BrowserFactory.Create(BrowserType.LIGHTWEIGHT));
+            Browser browser = browserView1.Browser;
+
+            // #1 Set browser initial size
+            browserView1.Browser.SetSize(1280, 1024);
+
+            // #2 Load web page and wait until web page is loaded completely.
+            ManualResetEvent resetEvent = new ManualResetEvent(false);
+            FinishLoadingFrameHandler listener = new FinishLoadingFrameHandler((object sender1, FinishLoadingEventArgs e1) =>
+            {
+                if (e1.IsMainFrame)
+                {
+                    resetEvent.Set();
+                }
+            });
+            browser.FinishLoadingFrameEvent += listener;
+            try
+            {
+                browser.LoadURL(toolStripAddress.Text.ToString());
+                resetEvent.WaitOne(new TimeSpan(0, 0, 45));
+            }
+            finally
+            {
+                browser.FinishLoadingFrameEvent -= listener;
+            }
+
+            // #3 Set the required document size.
+            JSValue documentHeight = browserView1.Browser.ExecuteJavaScriptAndReturnValue(
+                    "Math.max(document.body.scrollHeight, " +
+                    "document.documentElement.scrollHeight, document.body.offsetHeight, " +
+                    "document.documentElement.offsetHeight, document.body.clientHeight, " +
+                    "document.documentElement.clientHeight);");
+            JSValue documentWidth = browserView1.Browser.ExecuteJavaScriptAndReturnValue(
+                    "Math.max(document.body.scrollWidth, " +
+                    "document.documentElement.scrollWidth, document.body.offsetWidth, " +
+                    "document.documentElement.offsetWidth, document.body.clientWidth, " +
+                    "document.documentElement.clientWidth);");
+
+            int scrollBarSize = 25;
+
+            int viewWidth = (int)documentWidth.GetNumber() + scrollBarSize;
+            int viewHeight = (int)documentHeight.GetNumber() + scrollBarSize;
+
+            // #4 Register OnRepaint to get notifications
+            // about paint events. We expect that web page will be completely rendered twice:
+            // 1. When its size is updated.
+            // 2. When HTML content is loaded and displayed.
+            ManualResetEvent waitEvent = new ManualResetEvent(false);
+
+            DrawingView drawingView = (DrawingView)browserView1.GetImage();
+            browserView1.Browser.SetSize(viewWidth, viewHeight);
+            // #5 Wait until Chromium renders web page content.
+            //waitEvent.WaitOne();
+            // #6 Save Image of the loaded web page into a PNG file.
+
+            //Create Save file dialog
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "PNG files (*.PNG)|*.PNG|All files (*.*)|*.*";
+            saveFileDialog.FilterIndex = 1;
+            saveFileDialog.RestoreDirectory = true;
+
+            //Choose path and name to save the file
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                //Format name of file
+                string imageFileName = saveFileDialog.FileName.ToString().Replace("%.png%", "");
+                browserView1.GetImage().Save(imageFileName, ImageFormat.Png);
+            }
+            browser.Dispose();
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            browserView.Dispose();
         }
     }
 }
+
